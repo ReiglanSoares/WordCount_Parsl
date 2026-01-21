07e6f0636639337cd5c5bc0ba4094b93f2f287be
REIGLAN SOARES <reiglanlourence@gmail.com>
2026-01-21 12:53:42 -0300

    Update apps.py
    Refactor wordcount_bucketed to return a boolean and update reduce_bucket signature to include dependencies.

--- apps.py
+++ apps.py
@@ -5,6 +5,7 @@ from hashlib import md5
 @python_app
 def wordcount_bucketed(input_file, out_dir, B, idx):
     os.makedirs(out_dir, exist_ok=True)
+
     outs = [open(f"{out_dir}/map_{idx}_bucket_{b}.txt", "w") for b in range(B)]
 
     with open(input_file, "r") as f:
@@ -15,25 +16,24 @@ def wordcount_bucketed(input_file, out_dir, B, idx):
 
     for o in outs:
         o.close()
-      
-    return [f"{out_dir}/map_{idx}_bucket_{b}.txt" for b in range(B)]
+        
+    return True
 
 @python_app
-def reduce_bucket(f1, f2, out_path):
+def reduce_bucket(f1, f2, out_path, _dep1=None, _dep2=None):
     counts = {}
 
-    # merge do arquivo 1
     with open(f1, "r") as a:
         for line in a:
             k, v = line.split()
             counts[k] = counts.get(k, 0) + int(v)
 
-    # merge do arquivo 2
     with open(f2, "r") as b:
         for line in b:
             k, v = line.split()
             counts[k] = counts.get(k, 0) + int(v)
 
+    # grava resultado
     with open(out_path, "w") as o:
         for k, v in counts.items():
             o.write(f"{k} {v}\n")
